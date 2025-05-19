3ab5c70c9436822ddbee6bc1350e18e64dc6b2f6
Alinofal1 <alinofal405@gmail.com>
2025-05-20 01:58:05 +0300

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+      <h2>Dash Board</h2>
+      <h2>Welcome Back, Client</h2>
+    </div>
+  )
+}
+
+export default Navbar
